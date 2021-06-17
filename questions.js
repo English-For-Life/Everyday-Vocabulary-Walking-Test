4646d6173b802ca7aee52e9939d4b215bbdc81b6
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-17 14:47:28 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,67 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"He was completely drunk. I watched him _______ across the road and fall down.",
+        options:["stagger", "wander", "trip", "crawl"],
+        answer: 0
+    },
+    {
+        q:"It’s very pleasant for a tourist to _______ round a new city with no particular purpose or destination.",
+        options:["trip", "wander", "crawl", "stagger"],
+        answer: 1
+    },
+    {
+        q:"It was a lovely day so we decided to _______ in the park for an hour.",
+        options:["limp", "creep", "stroll", "stray"],
+        answer: 2
+    },
+    {
+        q:"His injured foot made him _______ badly.",
+        options:["stray", "stroll", "creep", "limp"],
+        answer: 3
+    },
+    {
+        q:"Be careful or you’ll _______ on this icy bit of pavement.",
+        options:["slip", "march", "dash", "trudge"],
+        answer: 0
+    },
+    {
+        q:"Everyone was asleep when I returned so I had to _______ to my room without making a noise.",
+        options:["limp", "creep", "stroll", "stray"],
+        answer: 1
+    },
+    {
+        q:"If you join the army, you’ll have to learn to _______ .",
+        options:["trudge", "stagger", "march", "slip"],
+        answer: 2
+    },
+    {
+        q:"Please, don’t _______ away from the main group or you’ll get lost.",
+        options:["limp", "creep", "stroll", "stray"],
+        answer: 3
+    },
+    {
+        q:"Before babies can walk, they can only _______ on their hands and knees.",
+        options:["crawl", "trip", "wander", "stagger"],
+        answer: 0
+    },
+    {
+        q:"I’m afraid someone will _______ over that piece of wood and fall.",
+        options:["crawl", "trip", "wander", "stagger"],
+        answer: 1
+    },
+    {
+        q:"It began to rain and we had to _______ into a shop to keep dry.",
+        options:["slip", "stray", "dash", "trudge"],
+        answer: 2
+    },
+    {
+        q:"The exhausted men had to _______ for five miles through the snow.",
+        options:["slip", "dash", "trudge", "trudge"],
+        answer: 3
+    }
+
+
+
+]
